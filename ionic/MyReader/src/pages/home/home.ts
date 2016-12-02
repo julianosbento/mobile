4c8ad0d983813e0fd9602c10a8f2a80873a13d71
Julianosantosb <js.bentocontato@gmail.com>
2016-12-02 17:04:19 -0200

    Criando primeiro projeto ionic My feed reader

---
+++ ionic/MyReader/src/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public feeds: Array<string>;
+  private url: string = "https://www.reddit.com/new.json";
+
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+
+    this.fetchContent();
+  }
+  fetchContent ():void {
+    let loading = this.loadingCtrl.create({
+      content: 'Fetching content...'
+    });
+
+    loading.present();
+
+    this.http.get(this.url).map(res => res.json())
+        .subscribe(data => {
+          this.feeds = data.data.children;
+          loading.dismiss();
+        });
+  }
+}
